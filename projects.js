09c13cb16747d53d8d5cc0b94f6228fb7962b697
mvonfuchs07 <mvonfuchs@outlook.com>
2026-07-09 09:55:48 -0700

    Fix syntax error in projects.js

--- projects.js
+++ projects.js
@@ -124,7 +124,7 @@ const PROJECTS = [
     ]
   },
    
-  {
+{
     id: "Axes",
     category: "other",
     title: "Axe Restoration",
@@ -143,5 +143,5 @@ const PROJECTS = [
       { type: "image", src: "media/images/axe2.jpg" },
       { type: "image", src: "media/images/axe3.jpg" },
     ]
-    }
+  }
 ];
